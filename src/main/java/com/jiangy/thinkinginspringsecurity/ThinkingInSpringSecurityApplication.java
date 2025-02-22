5de323341e90176805de16ececeb847097fdef61
jiangy <jiangliu0316@outlook.com>
2025-02-22 15:32:45 +0800

    feat: init project

---
+++ src/main/java/com/jiangy/thinkinginspringsecurity/ThinkingInSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jiangy.thinkinginspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkingInSpringSecurityApplication.class, args);
+    }
+
+}
