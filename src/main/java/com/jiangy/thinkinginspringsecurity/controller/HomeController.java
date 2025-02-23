a6e702c29b6eb004a9389792136086fb0fd0b98b
jiangy <jiangliu0316@outlook.com>
2025-02-23 17:33:33 +0800

    feat: add tailwindcss daisyui

--- src/main/java/com/jiangy/thinkinginspringsecurity/controller/HomeController.java
+++ src/main/java/com/jiangy/thinkinginspringsecurity/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.jiangy.thinkinginspringsecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("principal {}", principal);
+        model.addAttribute("currentUser", principal);
         return "index";
     }
 }
